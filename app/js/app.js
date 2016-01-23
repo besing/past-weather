9b5591691c7a92a22455598c3749169c5d97c93e
Benni Desktop <benni.singer@gmail.com>
2016-01-23 19:04:31 +0100

    - first try of combining the two $http services

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,19 @@
-angular.module('pastWeather', [])
-    .controller('IpCheckCtrl', function($scope, $http) {
-        $http.get('http://ipinfo.io/geo')
-            .then(function(response) { $scope.ipcheck = response.data });
+var app = angular.module('pastWeather', []);
+
+app.controller('IpCheckCtrl', function($scope, $http) {
+    request = $http.get('http://ipinfo.io/geo')
+        .then(function(response) {
+            $scope.ipcheck = response.data;
+            //return response;
+        });
+});
+
+app.controller('WeatherCtrl', function($scope, $http) {
+    request.then(function(data) {
+       var location = $scope.ipcheck.loc;
     });
+    var url = 'https://api.forecast.io/forecast/14224f71802d6102f9e3337b9ab049a5/' + location;
+    $http.get(url)
+    // location.loc)
+        .then(function(response) { $scope.weather = response.data });
+});
