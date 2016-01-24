b7be6986244a1f573170df62089a8d8eb7062c7e
Benni Desktop <benni.singer@gmail.com>
2016-01-24 18:11:05 +0100

    - working: switch from simple GET to JSONP, CORS was the issue - works now for static query-url now (location) - next step: fetch IP/location from first controller, append it to second controller

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,7 @@
 var app = angular.module('pastWeather', []);
 
-/*app.controller('IpCheckCtrl', function($scope, $http) {
-    request = $http.get('http://ipinfo.io/geo')
+app.controller('IpCheckCtrl', function($scope, $http) {
+    $http.get('http://ipinfo.io/geo')
         .then(function(response) {
             $scope.ipcheck = response.data;
             //return response;
@@ -9,31 +9,17 @@ var app = angular.module('pastWeather', []);
 });
 
 app.controller('WeatherCtrl', function($scope, $http) {
-    request.then(function(data) {
-       var location = $scope.ipcheck.loc;
-    });
-    var url = 'https://api.forecast.io/forecast/14224f71802d6102f9e3337b9ab049a5/' + location;
-    $http.get(url)
-    // location.loc)
-        .then(function(response) { $scope.weather = response.data });
-});*/
 
-app.controller('CombinedCtrl', function($scope, $http) {
-    var requestIp = $http.get('http://ipinfo.io/geo')
+    //request.then(function(data) {
+    //   var location = $scope.ipcheck.loc;
+    //});
+
+    //var url = 'https://api.forecast.io/forecast/14224f71802d6102f9e3337b9ab049a5/' + location;
+    var url = 'https://api.forecast.io/forecast/14224f71802d6102f9e3337b9ab049a5/53.5500,10.0000';
+    var callbackJsonp = '?callback=JSON_CALLBACK';
+    $http.jsonp(url + callbackJsonp)
         .then(function(response) {
-            $scope.ipcheck = response.data;
-            return response;
+            $scope.weather = response.data;
+            console.log('Response: ' + response.data);
         });
-
-    requestIp.then(function(data) {
-        var ort = $scope.ipcheck.loc;
-
-        var urlApi = 'https://api.forecast.io/forecast/14224f71802d6102f9e3337b9ab049a5/' + ort;
-        console.log('URL =' + urlApi);
-
-        //$http.get(urlApi)
-        $http.get('https://api.forecast.io/forecast/14224f71802d6102f9e3337b9ab049a5/53.5500,10.0000')
-            .then(function(response) { $scope.weather = response.data });
-            console.log('Response: ' + weather.timezone);
-    });
 });
