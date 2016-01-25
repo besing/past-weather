d4e08b6dbf6547d52ce58bda4d54e21957ffb62a
Benni Desktop <benni.singer@gmail.com>
2016-01-25 18:16:34 +0100

    - add a few comments, tiny up - unfortunately still not working, since 2nd Ctrl URL needs to be created before promise (async load not yet finished) (http://bit.ly/1OSTqSQ)

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,7 @@
 var app = angular.module('pastWeather', []);
 
+// === Factory for Sharing Data between controllers
+// ====== Tuts+: http://bit.ly/1PwcJXH
 app.factory('resultService', function() {
     var result;
     var resultService = {
@@ -13,23 +15,26 @@ app.factory('resultService', function() {
     return resultService;
 });
 
+// === 1st controller (IP Check)
 app.controller('IpCheckCtrl', function($scope, $http, resultService) {
     $http.get('http://ipinfo.io/geo')
         .then(function(response) {
             $scope.ipcheck = response.data;
             resultService.setResult(response.data);
-            console.log('Query 1:', response.data);
-            console.log('GetRes(1):', resultService.getResult());
+            //console.log('Query 1:', response.data);
+            //console.log('GetRes(1):', resultService.getResult());
         });
 });
 
+// === 2nd controller (use IP for Weather Data)
+// ====== (call resultService inside Promise of $http (http://bit.ly/1Pwdlww))
 app.controller('WeatherCtrl', function($scope, $http, resultService) {
-    var url = 'https://api.forecast.io/forecast/14224f71802d6102f9e3337b9ab049a5/53.5500,10.0000';
-    var callbackJsonp = '?callback=JSON_CALLBACK';
-    $http.jsonp(url + callbackJsonp)
+    var urlByLocation = 'https://api.forecast.io/forecast/14224f71802d6102f9e3337b9ab049a5/'
+        + resultService.getResult().loc;
+    $http.jsonp(urlByLocation + '?callback=JSON_CALLBACK')
     .then(function(response) {
         $scope.weather = response.data;
-        console.log('Query 2:', response.data);
-        console.log('GetRes(2):', resultService.getResult().city);
+        //console.log('Query 2:', response.data);
+        //console.log('GetRes(2):', resultService.getResult().city);
         });
 });
