ac29d3a4d6b13df9133489a119f23acf4f147c98
Benni Desktop <benni.singer@gmail.com>
2016-01-25 17:14:35 +0100

    - now working: fetch IP/location from first controller, append it to second controller - issue was: getResult() in 2nd controller was called outside of $http (needs to be called inside $http and in the promise then()) - next step: tidy up code, create comments

--- app/js/app.js
+++ app/js/app.js
@@ -1,25 +1,35 @@
 var app = angular.module('pastWeather', []);
 
-app.controller('IpCheckCtrl', function($scope, $http) {
+app.factory('resultService', function() {
+    var result;
+    var resultService = {
+        setResult: function(input) {
+            result = input;
+        },
+        getResult: function() {
+            return result;
+        }
+    };
+    return resultService;
+});
+
+app.controller('IpCheckCtrl', function($scope, $http, resultService) {
     $http.get('http://ipinfo.io/geo')
         .then(function(response) {
             $scope.ipcheck = response.data;
-            //return response;
+            resultService.setResult(response.data);
+            console.log('Query 1:', response.data);
+            console.log('GetRes(1):', resultService.getResult());
         });
 });
 
-app.controller('WeatherCtrl', function($scope, $http) {
-
-    //request.then(function(data) {
-    //   var location = $scope.ipcheck.loc;
-    //});
-
-    //var url = 'https://api.forecast.io/forecast/14224f71802d6102f9e3337b9ab049a5/' + location;
+app.controller('WeatherCtrl', function($scope, $http, resultService) {
     var url = 'https://api.forecast.io/forecast/14224f71802d6102f9e3337b9ab049a5/53.5500,10.0000';
     var callbackJsonp = '?callback=JSON_CALLBACK';
     $http.jsonp(url + callbackJsonp)
-        .then(function(response) {
-            $scope.weather = response.data;
-            console.log('Response: ' + response.data);
+    .then(function(response) {
+        $scope.weather = response.data;
+        console.log('Query 2:', response.data);
+        console.log('GetRes(2):', resultService.getResult().city);
         });
 });
