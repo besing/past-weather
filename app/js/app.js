0300e77ae65b345378b5b0885a5691269a66140b
Benni Desktop <benni.singer@gmail.com>
2016-02-01 23:23:20 +0100

    - new forecast.io API key

--- app/js/app.js
+++ app/js/app.js
@@ -9,7 +9,7 @@ app.controller('ComboCtrl', function($scope, $http) {
             $scope.ipCheck = response.data;
             return $http({
                 method: 'JSONP',
-                url: 'https://api.forecast.io/forecast/14224f71802d6102f9e3337b9ab049a5/' + $scope.ipCheck.loc + '?units=si&callback=JSON_CALLBACK'});
+                url: 'https://api.forecast.io/forecast/1d96cc3ef09464128ca17edb27b686c6/' + $scope.ipCheck.loc + '?units=si&lang=de&callback=JSON_CALLBACK'});
         })
         .then(function(response) {
             $scope.weatherInfo = response.data;
