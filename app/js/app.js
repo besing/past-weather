8341b0324e106d6d8227165f0150c2ef8056d22f
Benni Desktop <benni.singer@gmail.com>
2016-02-07 19:58:36 +0100

    - Implement Current-Weather-Visual, randomized (needs to have the count of imgs for each weather-type) - rename App - first basic CSS

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('pastWeather', []);
+var app = angular.module('wetterLuchs', []);
 
 //==== Controller with 2 API calls (dependend on e.a.) ("Promises-Chaining")
 //---- http://bit.ly/1PijnPx
@@ -7,6 +7,7 @@ app.controller('WeatherCtrl', function($scope, $http) {
 // 1st API Call (IP/Location)
     $http({method: 'GET', url: 'http://ipinfo.io/geo'})
     // TODO: add caching!
+
         .then(function (response) {
             $scope.ipCheck = response.data;
 // 2nd API Call (W. Forecast)
@@ -17,5 +18,57 @@ app.controller('WeatherCtrl', function($scope, $http) {
 
         .then(function(response) {
             $scope.weatherInfo = response.data;
+
+            var iconImgCount = 0;
+            switch (response.data.currently.icon) {
+                // current number of pictures for every weather state ('icon')
+                // (needs to be modified each time a picture gets added/removed)
+                // (automation would be nice but difficult since JS = client side
+                // and no access to files on the server (.jpg))
+                case 'clear-day':
+                    iconImgCount = 6;
+                    break;
+                case 'clear-night':
+                    iconImgCount = 4;
+                    break;
+                case 'cloudy':
+                    iconImgCount = 1;
+                    break;
+                case 'fog':
+                    iconImgCount = 4;
+                    break;
+                case 'partly-cloudy-day':
+                    iconImgCount = 2;
+                    break;
+                case 'partly-cloudy-night':
+                    iconImgCount = 1;
+                    break;
+                case 'rain':
+                    iconImgCount = 2;
+                    break;
+                case 'sleet':
+                    iconImgCount = 1;
+                    break;
+                case 'snow':
+                    iconImgCount = 3;
+                    break;
+                case 'wind':
+                    iconImgCount = 2;
+                    break;
+                default:
+                    console.log('Forecast Icon Error!');
+            }
+
+            var randIconImg;
+
+            if (iconImgCount == 0) {
+                console.log('No Forecast Icon -- no different images!');
+            } else if (iconImgCount == 1) {
+                randIconImg = 1;
+            } else {
+                randIconImg = Math.floor(Math.random() * iconImgCount + 1);
+            }
+
+            $scope.currentIconImgCount = randIconImg;
         });
 });
