48c53a8868804c3c72ea2d0d982b12b6fda3b3bd
Benni Desktop <benni.singer@gmail.com>
2016-02-07 18:56:26 +0100

    - Rename Controller - add a few comments - add current weather icon

--- app/js/app.js
+++ app/js/app.js
@@ -2,15 +2,19 @@ var app = angular.module('pastWeather', []);
 
 //==== Controller with 2 API calls (dependend on e.a.) ("Promises-Chaining")
 //---- http://bit.ly/1PijnPx
-app.controller('ComboCtrl', function($scope, $http) {
+app.controller('WeatherCtrl', function($scope, $http) {
+
+// 1st API Call (IP/Location)
     $http({method: 'GET', url: 'http://ipinfo.io/geo'})
-    // TODO: add caching
+    // TODO: add caching!
         .then(function (response) {
             $scope.ipCheck = response.data;
+// 2nd API Call (W. Forecast)
             return $http({
                 method: 'JSONP',
                 url: 'https://api.forecast.io/forecast/1d96cc3ef09464128ca17edb27b686c6/' + $scope.ipCheck.loc + '?units=si&lang=de&callback=JSON_CALLBACK'});
         })
+
         .then(function(response) {
             $scope.weatherInfo = response.data;
         });
